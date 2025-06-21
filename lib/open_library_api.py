7b3604cd4a625e1517264729673238457a1b08ea
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-06-21 19:27:16 +0300

    added search input with error handling for Open Library API

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -6,22 +6,17 @@ class Search:
 
     def get_search_results(self):
         search_term = "the lord of the rings"
-
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
-        # formats the list into a comma separated string
-        # output: "title,author_name"
         fields_formatted = ",".join(fields)
         limit = 1
 
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-
         response = requests.get(URL)
         return response.content
 
     def get_search_results_json(self):
         search_term = "the lord of the rings"
-
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
         fields_formatted = ",".join(fields)
@@ -39,19 +34,18 @@ class Search:
         limit = 1
 
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-
         response = requests.get(URL).json()
-        response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
-        return response_formatted
+
+        try:
+            title = response['docs'][0]['title']
+            author = response['docs'][0]['author_name'][0]
+            return f"Title: {title}\nAuthor: {author}"
+        except (IndexError, KeyError):
+            return "No results found for that title."
 
 
-# results = Search().get_search_results()
-# print(results)
-
-# results_json = Search().get_search_results_json()
-# print(json.dumps(results_json, indent=1))
-
-search_term = input("Enter a book title: ")
-result = Search().get_user_search_results(search_term)
-print("Search Result:\n")
-print(result)
+if __name__ == "__main__":
+    search_term = input("Enter a book title: ")
+    result = Search().get_user_search_results(search_term)
+    print("Search Result:\n")
+    print(result)
